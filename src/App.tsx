a6a0d92136ab810482bb4ca542dfa1876b83dfc6
seunghwan <sun1238799@naver.com>
2023-07-09 11:03:22 +0900

    chore: GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { getClient } from './queryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { routes } from '@/Routes';
-
+import GlobalStyle from './GlobalStyle';
 export default function App() {
   const queryClient = getClient();
   const elem = useRoutes(routes);
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       {elem}
       <ReactQueryDevtools />
     </QueryClientProvider>
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
 const Main = () => {
-  return <div>main</div>;
+  return <Mains>main</Mains>;
 };
 
 export default Main;
+
+const Mains = styled.div`
+  font-size: 3rem;
+`;
