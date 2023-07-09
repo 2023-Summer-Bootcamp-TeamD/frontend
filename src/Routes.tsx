0d9db640f8a0888f1fbb0e487954ce16c9e5aef1
seunghwan <sun1238799@naver.com>
2023-07-09 09:39:40 +0900

    chore: 기본 환경세팅

---
+++ src/Routes.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-refresh/only-export-components */
+import { lazy } from 'react';
+import GlobalLayout from '@/pages/_layout';
+
+const MainPage = lazy(() => import('@/pages/Main'));
+
+export const routes = [
+  {
+    path: '/',
+    element: <GlobalLayout />,
+    children: [{ path: '/', element: <MainPage /> }],
+  },
+];
+
+export const pages = [{ route: '/' }];
