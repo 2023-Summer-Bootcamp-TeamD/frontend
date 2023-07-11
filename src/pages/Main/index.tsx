6c4e6b48ce1cb42b55ce5cf34c737563c1d2cc6a
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-07-11 17:25:59 +0900

    feat: Header 퍼블리싱 및 assets 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import NavBar from '@/common/NavBar';
 
 const Main = () => {
-  return <Mains>main</Mains>;
+  return (
+    <Mains>
+      <NavBar />
+      <BackBoard />
+    </Mains>
+  )
 };
 
 export default Main;
 
 const Mains = styled.div`
   font-size: 3rem;
+  height: 100vh;
+  width: 100vw;
+  background-image: linear-gradient(to bottom, #FAF1E5 70vh, #C9CB81 40vh);
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
+
+
+const BackBoard = styled.div`
+  box-sizing: border-box;
+
+  width: 70vw;
+  height: 65vh;
+  background: #1C3B3E;
+  border: 15px solid #8E5501;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+`
