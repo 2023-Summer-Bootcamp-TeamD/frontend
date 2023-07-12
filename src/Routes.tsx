9e803080bb0900c97fbd04b173850d1358c7fc99
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2023-07-12 15:09:02 +0900

    design : 칠판 배경 퍼블리싱

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,12 +3,15 @@ import { lazy } from 'react';
 import GlobalLayout from '@/pages/_layout';
 
 const MainPage = lazy(() => import('@/pages/Main'));
-
+const EntryRoomPage = lazy(() => import('@/pages/EntryRoom'));
 export const routes = [
   {
     path: '/',
     element: <GlobalLayout />,
-    children: [{ path: '/', element: <MainPage /> }],
+    children: [
+      { path: '/', element: <MainPage /> },
+      { path: '/entryRoom', element: <EntryRoomPage /> },
+    ],
   },
 ];
 
---
+++ src/pages/EntryRoom/index.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import chattingImg from '@/assets/Chatter.png';
+import CompassImg from '@/assets/DoodleCompass.png';
+import FunctionMathImg from '@/assets/DoodleFunctionMath.png';
+import FireExtinguisherImg from '@/assets/FireExtinguisher.png';
+import TeachingImg from '@/assets/Teaching.png';
+const EntryRoom = () => {
+  return (
+    <Wrap>
+      <FireExtinguisher />
+      <Teaching />
+      <ChalkBoard>
+        {' '}
+        <DoodleCompass />
+        <DoodleMath />
+        <DoodleChatting />
+      </ChalkBoard>
+    </Wrap>
+  );
+};
+
+export default EntryRoom;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  background-image: linear-gradient(
+    to bottom,
+    #faf1e5 0%,
+    #faf1e5 70%,
+    #c9cb81 70%,
+    #c9cb81 100%
+  );
+`;
+
+const FireExtinguisher = styled.img.attrs({
+  src: `${FireExtinguisherImg}`,
+})`
+  position: absolute;
+  right: 2rem;
+  bottom: 0;
+  width: 30rem;
+`;
+const Teaching = styled.img.attrs({
+  src: `${TeachingImg}`,
+})`
+  position: absolute;
+  bottom: 0;
+  width: 50rem;
+`;
+
+const ChalkBoard = styled.div`
+  box-sizing: border-box;
+  width: 70vw;
+  height: 65vh;
+  background: #1c3b3e;
+  border: 15px solid #8e5501;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const DoodleCompass = styled.img.attrs({
+  src: `${CompassImg}`,
+})`
+  position: absolute;
+  left: 12rem;
+  top: 2rem;
+  width: 5%;
+`;
+
+const DoodleMath = styled.img.attrs({
+  src: `${FunctionMathImg}`,
+})`
+  position: absolute;
+  left: 1rem;
+  top: 0.7rem;
+  width: 10%;
+`;
+
+const DoodleChatting = styled.img.attrs({
+  src: `${chattingImg}`,
+})`
+  position: absolute;
+  right: 2rem;
+  bottom: 3rem;
+  width: 10%;
+`;
