7830168e23b9d7da24b460a01d2b609acda3b8ed
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2023-07-13 15:33:29 +0900

    feat : 카테고리 퍼블리싱 추가

--- src/pages/DrawingResult/index.tsx
+++ src/pages/DrawingResult/index.tsx
@@ -5,6 +5,9 @@ import DrawingHeader from '@/common/DrawingHeader ';
 import chattingImg from '@/assets/Chatter.png';
 import CompassImg from '@/assets/DoodleCompass.png';
 import FunctionMathImg from '@/assets/DoodleFunctionMath.png';
+import FoodImg from '@/assets/Food.png';
+import PersonImg from '@/assets/Person.png';
+import AnimalImg from '@/assets/Animal.png';
 const DrawingREsult = () => {
   return (
     <Wrap>
@@ -14,6 +17,20 @@ const DrawingREsult = () => {
         <DoodleCompass />
         <DoodleMath />
         <DoodleChatting />
+        <CategoryWrap>
+          <Category>
+            <CategoryImg src={FoodImg} />
+            <CategoryTitle>음식</CategoryTitle>
+          </Category>
+          <Category>
+            <CategoryImg src={AnimalImg} />
+            <CategoryTitle>동물</CategoryTitle>
+          </Category>
+          <Category>
+            <CategoryImg src={PersonImg} />
+            <CategoryTitle>인물</CategoryTitle>
+          </Category>
+        </CategoryWrap>
       </Blackboard>
     </Wrap>
   );
@@ -70,6 +87,42 @@ const DoodleChatting = styled.img.attrs({
   bottom: 3rem;
   width: 10rem;
 `;
+const CategoryWrap = styled.div`
+  width: 100%;
+  height: 17rem;
+  //background: #41d319;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Category = styled.button`
+  width: 17rem;
+  height: 17rem;
+  border-radius: 20px;
+  border: 0.15rem solid #fff;
+  background-color: transparent;
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.18);
+    cursor: pointer;
+  }
+`;
+const CategoryImg = styled.img`
+  height: 15rem;
+  width: 15rem;
+  //background-color: #0088ff;
+`;
+const CategoryTitle = styled.label`
+  font-size: 4rem;
+  color: #fff;
+  // background-color: aliceblue;
+  position: absolute;
+  bottom: 2rem;
+`;
+
 const FireExtinguisher = styled.img.attrs({
   src: `${FireExtinguisherImg}`,
 })`
