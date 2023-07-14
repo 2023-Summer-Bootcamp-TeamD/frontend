848e07ea4e8d6c41e292ca84e649e451fa7d8014
Seo_KeunJae <tjrmswo506@naver.com>
2023-07-14 17:59:31 +0900

    refactor: 방 만드는 페이지 찐 최종

--- src/pages/CreatingSpecificRooms/index.tsx
+++ src/pages/CreatingSpecificRooms/index.tsx
@@ -11,7 +11,7 @@ import Animal from '@/assets/Animal.png';
 import Person from '@/assets/Person.png';
 import Header from '../Header';
 const CreatingRooms = () => {
-  const rommElement = [
+  const roomElement = [
     { image: Food, id: '음식' },
     { image: Animal, id: '동물' },
     { image: Person, id: '인물' },
@@ -19,40 +19,24 @@ const CreatingRooms = () => {
   ];
   const [personnel, setPersonnel] = useState(2);
   const [seconds, setSeconds] = useState(10);
-  const increaseAdmission = () => {
-    if (personnel < 2) {
-      setPersonnel(2);
-    } else if (personnel >= 2 && personnel < 8) {
+  const increasePersonnel = () => {
+    if (personnel < 8) {
       setPersonnel(personnel + 1);
-    } else if (personnel > 8) {
-      setPersonnel(8);
     }
   };
-  const decreaseAdmission = () => {
-    if (personnel <= 2) {
-      setPersonnel(2);
-    } else if (personnel > 2 && personnel <= 8) {
+  const decreasePersonnel = () => {
+    if (personnel > 2) {
       setPersonnel(personnel - 1);
-    } else if (personnel > 8) {
-      setPersonnel(8);
     }
   };
   const increaseSeconds = () => {
-    if (seconds < 10) {
-      setSeconds(10);
-    } else if (seconds >= 10 && seconds < 60) {
+    if (seconds < 60) {
       setSeconds(seconds + 10);
-    } else if (seconds > 60) {
-      setSeconds(60);
     }
   };
   const decreaseSeconds = () => {
-    if (seconds <= 10) {
-      setSeconds(0);
-    } else if (seconds > 10 && seconds <= 60) {
+    if (seconds > 10) {
       setSeconds(seconds - 10);
-    } else if (seconds > 60) {
-      setSeconds(60);
     }
   };
   return (
@@ -67,7 +51,7 @@ const CreatingRooms = () => {
         <img className="CompassImg" src={DoodleCompass} alt="컴퍼스낙서" />
       </DoodleContainer>
       <ButtonContainer>
-        {rommElement.map((item, index) => (
+        {roomElement.map((item, index) => (
           <div key={index}>
             <img src={item.image} alt={item.id} />
             <div>{item.id}</div>
@@ -75,34 +59,43 @@ const CreatingRooms = () => {
         ))}
       </ButtonContainer>
       <Blackboard />
-      <UIContainerRow>
-        <UIContainerColumn>
-          <NumberOfAdmissions>입장 인원</NumberOfAdmissions>
-          <NumberOfAdmissionsRow>
-            <UpButton onClick={increaseAdmission}>&lt;</UpButton>
-            <Personnels>{personnel}명</Personnels>
-            <DownButton onClick={decreaseAdmission}>&gt;</DownButton>
-          </NumberOfAdmissionsRow>
-        </UIContainerColumn>
-        <UIContainerColumn>
-          <NickName>닉네임</NickName>
-          <InputNickName
+      <UIContainer>
+        <div>
+          <div className="NumberOfAdmissions">입장 인원</div>
+          <div className="NumberOfAdmissionsRow">
+            <button className="IncreasButton" onClick={increasePersonnel}>
+              &lt;
+            </button>
+            <div className="Personnels">{personnel}명</div>
+            <button className="DecreasButton" onClick={decreasePersonnel}>
+              &gt;
+            </button>
+          </div>
+        </div>
+        <div>
+          <div className="NickName">닉네임</div>
+          <input
+            className="InputNickName"
             type="text"
             placeholder="닉네임을 입력해주세요"
             required
             maxLength={5}
           />
-          <CreatingRoomButton>방 만들기</CreatingRoomButton>
-        </UIContainerColumn>
-        <UIContainerColumn>
-          <TimeLimitPerRound>라운드 당 제한시간</TimeLimitPerRound>
-          <NumberOfAdmissionsRow>
-            <UpButton onClick={increaseSeconds}>&lt;</UpButton>
-            <Seconds>{seconds}s</Seconds>
-            <DownButton onClick={decreaseSeconds}>&gt;</DownButton>
-          </NumberOfAdmissionsRow>
-        </UIContainerColumn>
-      </UIContainerRow>
+          <button className="CreatingRoomButton">방 만들기</button>
+        </div>
+        <div>
+          <div className="TimeLimitPerRound">라운드 당 제한시간</div>
+          <div className="TimeLimitPerRoundRow">
+            <button className="IncreaseSeconds" onClick={increaseSeconds}>
+              &lt;
+            </button>
+            <div className="Seconds">{seconds}s</div>
+            <button className="DecreaseSeconds" onClick={decreaseSeconds}>
+              &gt;
+            </button>
+          </div>
+        </div>
+      </UIContainer>
       <ChatterImg src={Chatter} alt="떠든사람" />
       <TeachingImg src={Teaching} alt="교탁" />
       <FireExtinguisherImg src={FireExtinguisher} alt="소화기" />
@@ -124,29 +117,7 @@ const Admissions = styled.div`
   width: 100vw;
   position: relative;
 `;
-const Blackboard = styled.div`
-  border-box;
-  width: 70vw;
-  height: 65vh;
-  background: #1C3B3E;
-  border: 15px solid #8E5501;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 20px;
-  zIndex: 2;
-  position: relative;
-  bottom: 2rem;
-`;
-const TeachingImg = styled.img`
-  position: absolute;
-  width: 30vw;
-  bottom: 0;
-`;
-const ChatterImg = styled.img`
-  position: absolute;
-  top: 21em;
-  right: 10em;
-  z-index: 1;
-`;
+
 const DoodleContainer = styled.div`
   z-index: 2;
   top: 10em;
@@ -154,26 +125,20 @@ const DoodleContainer = styled.div`
     position: absolute;
   }
   > .FunctionMathImg {
-    left: 17%;
+    left: 16em;
     top: 12em;
   }
   > .CompassImg {
-    left: 28%;
+    left: 25em;
     top: 12em;
   }
-`;
-const FireExtinguisherImg = styled.img`
-  width: 10wh;
-  height: 15vh;
-  position: absolute;
-  bottom: 0;
-  right: 2em;
 `;
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   position: absolute;
   top: 9.8em;
+
   > div {
     display: flex;
     flex-direction: column;
@@ -205,130 +170,161 @@ const ButtonContainer = styled.div`
     background-color: rgba(255, 255, 255, 0.2);
   }
 `;
-const UIContainerRow = styled.div`
+const UIContainer = styled.div`
   position: absolute;
   top: 14.5em;
-  left: 18.8em;
+  left: 17.6em;
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   align-items: center;
+
   > div {
     display: flex;
     flex-direction: column;
     text-align: center;
     margin-right: 3em;
     z-index: 1;
+    align-items: center;
+    .NumberOfAdmissions {
+      text-align: center;
+      color: white;
+      font-size: 2em;
+      position: relative;
+    }
   }
-
-  > div:nth-child(1) {
-    position: relative;
-    right: 7em;
+  .NumberOfAdmissionsRow {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    > .IncreasButton,
+    .DecreasButton {
+      display: flex;
+      align-items: center;
+      font-size: 2em;
+      border: 2px solid white;
+      height: 1.5em;
+      color: white;
+      position: relative;
+      top: 1.5rem;
+      background-color: transparent;
+      border-radius: 50%;
+      padding: 0.3em;
+      margin-right: 1rem;
+      margin-left: 1rem;
+    }
+    > .Personnels {
+      font-size: 6em;
+      display: inline-block;
+      position: relative;
+      bottom: 0.7rem;
+      color: white;
+      text-align: center;
+    }
   }
-
-  > div:nth-child(3) {
+  > div:nth-child(2) > .NickName {
+    font-size: 2em;
+    color: white;
+    margin-bottom: 2rem;
+  }
+  > div:nth-child(2) > .InputNickName {
+    width: 20vw;
+    font-family: 'Uhbee mysen';
+    border-radius: 2em;
+    background-color: rgba(255, 255, 255, 0.3);
+    font-size: 2em;
+    text-align: center;
+    color: white;
+    border: 0.2rem solid white;
+    &::placeholder {
+      color: white;
+      font-size: 0.9em;
+    }
+    &:focus {
+      background-color: rgba(255, 255, 255, 0);
+    }
+  }
+  > div:nth-child(2) > .CreatingRoomButton {
+    width: 10vw;
+    font-family: 'Uhbee mysen';
+    border-radius: 2em;
+    background-color: rgba(255, 255, 255, 0);
+    font-size: 2.5em;
+    text-align: center;
+    color: white;
+    border: 0.2rem solid white;
+    margin-top: 1rem;
+    margin-bottom: auto;
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.3);
+    }
+  }
+  > div:nth-child(3) > .TimeLimitPerRound {
+    text-align: center;
+    font-family: Uhbee mysen;
+    color: white;
+    font-size: 2em;
     position: relative;
-    left: 5em;
+  }
+  > div:nth-child(3) > .TimeLimitPerRoundRow {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    > .IncreaseSeconds,
+    .DecreaseSeconds {
+      display: flex;
+      align-items: center;
+      font-size: 2em;
+      border: 2px solid white;
+      height: 1.5em;
+      color: white;
+      position: relative;
+      top: 1.5rem;
+      background-color: transparent;
+      border-radius: 50%;
+      padding: 0.3em;
+      margin-right: 1rem;
+      margin-left: 1rem;
+    }
+    .Seconds {
+      font-size: 6em;
+      display: inline-block;
+      position: relative;
+      bottom: 0.7rem;
+      color: white;
+      text-align: center;
+    }
   }
 `;
-const UIContainerColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  margin-right: 3em;
+
+const Blackboard = styled.div`
+  border-box;
+  width: 70vw;
+  height: 65vh;
+  background: #1C3B3E;
+  border: 15px solid #8E5501;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  zIndex: 2;
+  position: relative;
+  bottom: 2rem;
+`;
+
+const ChatterImg = styled.img`
+  position: absolute;
+  top: 21em;
+  right: 10em;
   z-index: 1;
 `;
-const NumberOfAdmissions = styled.div`
-  text-align: center;
-  font-family: Uhbee mysen;
-  color: white;
-  font-size: 2em;
+const TeachingImg = styled.img`
+  position: absolute;
+  width: 30vw;
+  bottom: 0;
 `;
-const NumberOfAdmissionsRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`;
-const UpButton = styled.button`
-  display: flex;
-  align-items: center;
-  font-size: 2em;
-  border: 2px solid white;
-  height: 1.5em;
-  color: white;
-  position: relative;
-  top: 1.5rem;
-  background-color: transparent;
-  border-radius: 50%;
-  padding: 0.3em;
-  margin-right: 1rem;
-`;
-const DownButton = styled.button`
-  display: flex; /* 요소를 수직으로 가운데 정렬 */
-  align-items: center;
-  font-size: 2em;
-  border: 2px solid white;
-  height: 1.5em;
-  color: white;
-  position: relative;
-  top: 1.5rem;
-  background-color: transparent;
-  border-radius: 50%;
-  padding: 0.3em;
-  margin-left: 1rem;
-`;
-const Personnels = styled.span`
-  font-size: 6em;
-  display: inline-block;
-  position: relative;
-  bottom: 0.7rem;
-  color: white;
-  text-align: center;
-`;
-const NickName = styled.span`
-  font-size: 2em;
-  color: white;
-  margin-bottom: 2rem;
-`;
-const InputNickName = styled.input`
-  font-family: 'Uhbee mysen';
-  border-radius: 2em;
-  background-color: rgba(255, 255, 255, 0.3);
-  font-size: 2em;
-  text-align: center;
-  color: white;
-  border: 0.2rem solid white;
-  &::placeholder {
-    color: white;
-    font-size: 1em;
-  }
-  &:focus {
-    background-color: rgba(255, 255, 255, 0);
-  }
-`;
-const CreatingRoomButton = styled.button`
-  font-family: 'Uhbee mysen';
-  border-radius: 2em;
-  background-color: rgba(255, 255, 255, 0);
-  font-size: 2em;
-  text-align: center;
-  color: white;
-  border: 0.2rem solid white;
-  margin-top: 2rem;
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.3);
-  }
-`;
-const TimeLimitPerRound = styled.span`
-  text-align: center;
-  font-size: 2em;
-  color: white;
-`;
-const Seconds = styled.span`
-  font-size: 6em;
-  display: inline-block;
-  position: relative;
-  bottom: 0.7rem;
-  color: white;
-  text-align: center;
+const FireExtinguisherImg = styled.img`
+  width: 10wh;
+  height: 15vh;
+  position: absolute;
+  bottom: 0;
+  right: 2em;
 `;
