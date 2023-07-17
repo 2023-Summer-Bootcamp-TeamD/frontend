32d14dff801b41367304339c1183c6c4aa4caab2
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2023-07-17 14:40:43 +0900

    rename :버튼,라벨  파일명 수정

--- src/pages/EntryRoom/index.tsx
+++ src/pages/EntryRoom/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { styled } from 'styled-components';
 import chattingImg from '@/assets/Chatter.png';
 import CompassImg from '@/assets/DoodleCompass.png';
