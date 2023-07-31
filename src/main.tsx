d24133a35468e53821cd87174b985c289d46b459
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-07-31 17:23:20 +0900

    design: 방 생성 페이지 api 호출 로딩 구현

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-import { worker } from './mocks/worker';
+// import { worker } from './mocks/worker';
 
-if (import.meta.env.DEV) {
-  worker.start();
-}
+// if (import.meta.env.DEV) {
+//   worker.start();
+// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
